f54b23fe0e19cea30568e634d17292660505c3c6
LucasManoelDuarte <84089272+LucasManoelDuarte@users.noreply.github.com>
2021-05-12 16:01:19 -0300

    Add files via upload

---
+++ inicial-de-kanto/README.markdown
@@ -0,0 +1,5 @@
+# Inicial de Kanto
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Lucas_MDuarte/pen/YzNPLNg](https://codepen.io/Lucas_MDuarte/pen/YzNPLNg).
+
+
---
+++ inicial-de-kanto/dist/script.js
@@ -0,0 +1,20 @@
+/* Foi utilizado um vídeo do canal Mixeli (https://www.youtube.com/user/elimanene) para conseguir as imagens! */
+function alteraInicialGrama()
+{
+  document.getElementById("escolhaSeuInicial").src = "https://i.pinimg.com/564x/7c/2b/20/7c2b2010e0adef0a8dc7851114461582.jpg";
+}
+
+function alteraInicialFogo() 
+{
+  document.getElementById("escolhaSeuInicial").src = "https://i.pinimg.com/564x/9e/a3/3b/9ea33b3e847f3e38674a6bda019a8859.jpg";
+}
+
+function alteraInicialAgua()
+{
+  document.getElementById("escolhaSeuInicial").src = "https://i.pinimg.com/564x/b6/14/9e/b6149edffdad893a0fa04472ecff1d49.jpg";
+}
+
+function escolherDeNovo()
+{
+  document.getElementById("escolhaSeuInicial").src = "https://i.pinimg.com/564x/d8/73/2c/d8732c1db367257a7e6425f71faa1d3b.jpg";
+}
